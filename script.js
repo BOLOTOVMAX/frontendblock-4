23f88de7b02dae7fab15e459e525f20123e8d2ac
BOLOTOVMAX <bolotovmax1983@mail.ru>
2023-10-22 19:21:01 +0300

    oneCommit

--- script.js
+++ script.js
@@ -40,3 +40,39 @@
 //             elems[i].innerHTML = i+1;
 //         }
 // }
+
+
+//Задача 1
+function buttonClick(){
+        let elem = document.getElementById('elem');
+        elem.innerHTML = '<span >Куку! А я</span>,<strong>жирный</strong>!';
+}
+
+//Задача 2
+function buttonClickTwo(){
+    let elems = document.getElementById('elems');
+    elems.innerHTML = '<h3>Абзац превратился в h3</h3>';
+}
+
+//Задача 3
+function buttonClickThree(){
+    let text = document.getElementById('text');
+    text.innerHTML = '<h3>При нажатии на кнопку текст в абзаце станет h3, но текст останется.</h3>';
+}
+
+//Задача 4
+function func2(){
+    let elems1 = document.getElementById('input1');
+    let elems2 = document.getElementById('input2');
+    let result = document.getElementById('result');
+    result.innerHTML = Number(elems1.value) + Number(elems2.value) 
+    return result
+    }
+
+ //Задача 5
+function func(){
+            let elems = document.querySelectorAll('p');
+             for(let i = 0;i<elems.length;i++){
+                 elems[i].innerHTML = i;
+             }
+            }
